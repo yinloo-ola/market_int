a3fd7a3bc3613b5d85caee489f1d2a05295627db
Tan Yin Loo <tianhai82@yahoo.com>
2024-10-13 22:57:37 +0800

    refactor, add comments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
+// Main function for the market data application.
 mod marketdata {
-    pub mod client;
+    // Client for fetching market data.
+    pub mod api_caller;
+    // Response structures for market data.
     pub mod response;
 }
+// HTTP client module.
 mod http {
+    // HTTP client implementation.
     pub mod client;
 }
+// Data models.
 mod model;
-mod pull_quotes;
+// Pull quotes functionality.
+mod quotes;
+// Data storage module.
 mod store {
+    // Candle data storage.
     pub mod candle;
+    // SQLite database interaction.
     pub mod sqlite;
 }
 
 use clap::{Parser, Subcommand};
 use dotenv::dotenv;
-use std::error::Error;
+use thiserror::Error;
 
+// Command-line argument parser.
 #[derive(Parser, Debug)]
 #[command(about, long_about = None)]
 struct Args {
@@ -23,45 +34,62 @@ struct Args {
     command: Commands,
 }
 
+// Subcommands for the application.
 #[derive(Subcommand, Debug)]
 enum Commands {
+    // Pull quotes for specified symbols.
     PullQuotes {
         symbols_file_path: String,
     },
+    // Calculate Average True Range (ATR).
     CalculateAtr {
         symbols_file_path: String,
         atr_percentile: f64,
     },
+    // Pull option chain data.
     PullOptionChain {
         symbols_file_path: String,
         side: String,
     },
 }
 
+#[derive(Error, Debug)]
+pub enum MarketDataError {
+    #[error("Database error: {0}")]
+    DatabaseError(#[from] rusqlite::Error),
+    #[error("Quotes error: {0}")]
+    QuotesError(#[from] quotes::QuotesError),
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("Other error: {0}")]
+    OtherError(String),
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+// Main function entry point.
+async fn main() -> Result<(), MarketDataError> {
     dotenv().ok();
 
     let args = Args::parse();
 
-    let conn = store::sqlite::init_sqlite_connection()?;
+    let conn = store::sqlite::init_connection()?;
 
     match args.command {
         Commands::PullQuotes { symbols_file_path } => {
-            pull_quotes::pull_quotes(&symbols_file_path, conn).await?;
+            quotes::pull_and_save(&symbols_file_path, conn).await?;
             Ok(())
         }
         Commands::CalculateAtr {
-            symbols_file_path,
-            atr_percentile,
+            symbols_file_path: _,
+            atr_percentile: _,
         } => {
             todo!()
         }
         Commands::PullOptionChain {
-            symbols_file_path,
-            side,
+            symbols_file_path: _,
+            side: _,
         } => {
-            todo!();
+            todo!()
         }
     }
 }
--- src/marketdata/response.rs
+++ src/marketdata/response.rs
@@ -1,57 +1,61 @@
 use serde::Deserialize;
 
+// Response structure for market status.
 #[derive(Deserialize, Debug)]
 pub struct MarketStatus {
-    pub s: String,
-    pub status: Vec<String>,
-    pub errmsg: Option<String>,
+    pub s: String,              // Status code.
+    pub status: Vec<String>,    // Market status.
+    pub errmsg: Option<String>, // Error message (if any).
 }
 
+// Response structure for daily candles.
 #[derive(Debug, Deserialize)]
 pub struct DailyCandles {
-    pub s: String,
-    pub c: Vec<f64>,
-    pub h: Vec<f64>,
-    pub l: Vec<f64>,
-    pub o: Vec<f64>,
-    pub t: Vec<u32>,
-    pub v: Vec<u32>,
-    pub errmsg: Option<String>,
+    pub s: String,              // Status code.
+    pub c: Vec<f64>,            // Close prices.
+    pub h: Vec<f64>,            // High prices.
+    pub l: Vec<f64>,            // Low prices.
+    pub o: Vec<f64>,            // Open prices.
+    pub t: Vec<u32>,            // Timestamps.
+    pub v: Vec<u32>,            // Volumes.
+    pub errmsg: Option<String>, // Error message (if any).
 }
 
+// Response structure for bulk candles.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BulkCandles {
-    pub s: String,
-    pub symbol: Vec<String>,
-    pub o: Vec<f64>,
-    pub h: Vec<f64>,
-    pub l: Vec<f64>,
-    pub c: Vec<f64>,
-    pub v: Vec<u32>,
-    pub t: Vec<u32>,
-    pub errmsg: Option<String>,
+    pub s: String,              // Status code.
+    pub symbol: Vec<String>,    // Symbols.
+    pub o: Vec<f64>,            // Open prices.
+    pub h: Vec<f64>,            // High prices.
+    pub l: Vec<f64>,            // Low prices.
+    pub c: Vec<f64>,            // Close prices.
+    pub v: Vec<u32>,            // Volumes.
+    pub t: Vec<u32>,            // Timestamps.
+    pub errmsg: Option<String>, // Error message (if any).
 }
 
+// Response structure for option chain data.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct OptionChain {
-    pub s: String,
-    pub option_symbol: Vec<String>,
-    pub underlying: Vec<String>,
-    pub expiration: Vec<u32>,
-    pub side: Vec<String>,
-    pub strike: Vec<f64>,
-    pub dte: Vec<u32>,
-    pub updated: Vec<u32>,
-    pub bid: Vec<f64>,
-    pub bid_size: Vec<u32>,
-    pub mid: Vec<f64>,
-    pub ask: Vec<f64>,
-    pub ask_size: Vec<u32>,
-    pub last: Vec<f64>,
-    pub open_interest: Vec<u32>,
-    pub volume: Vec<u32>,
-    pub underlying_price: Vec<f64>,
-    pub errmsg: Option<String>,
+    pub s: String,                  // Status code.
+    pub option_symbol: Vec<String>, // Option symbols.
+    pub underlying: Vec<String>,    // Underlying symbols.
+    pub expiration: Vec<u32>,       // Expiration timestamps.
+    pub side: Vec<String>,          // Call or Put.
+    pub strike: Vec<f64>,           // Strike prices.
+    pub dte: Vec<u32>,              // Days to expiration.
+    pub updated: Vec<u32>,          // Updated timestamps.
+    pub bid: Vec<f64>,              // Bid prices.
+    pub bid_size: Vec<u32>,         // Bid sizes.
+    pub mid: Vec<f64>,              // Mid prices.
+    pub ask: Vec<f64>,              // Ask prices.
+    pub ask_size: Vec<u32>,         // Ask sizes.
+    pub last: Vec<f64>,             // Last traded prices.
+    pub open_interest: Vec<u32>,    // Open interests.
+    pub volume: Vec<u32>,           // Volumes.
+    pub underlying_price: Vec<f64>, // Underlying prices.
+    pub errmsg: Option<String>,     // Error message (if any).
 }
