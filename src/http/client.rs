f36ffc474861fe5008ff0f62b967e42dfd8d0909
Tan Yin Loo <tianhai82@yahoo.com>
2024-10-19 17:30:27 +0800

    refactor a bit more

--- src/http/client.rs
+++ src/http/client.rs
@@ -1,9 +1,10 @@
-use ratelimit::Ratelimiter;
-use reqwest;
+use reqwest::{
+    self,
+    header::{HeaderMap, HeaderValue, InvalidHeaderValue},
+    RequestBuilder,
+};
 use serde::de::DeserializeOwned;
-use std::env;
-use std::sync::Arc;
-use std::time::Duration;
+use std::{collections::HashMap, sync::Arc};
 use thiserror::Error;
 
 // Base URL for the market data API.
@@ -12,19 +13,6 @@ const BASE_URL: &str = "https://api.marketdata.app/";
 // Shared HTTP client instance.
 lazy_static::lazy_static! {
     static ref CLIENT: Arc<reqwest::Client> = Arc::new(reqwest::Client::new());
-}
-
-// Rate limit: 5 requests per second.
-const TOKEN_PER_SEC: u64 = 1;
-// Rate limiter instance.
-lazy_static::lazy_static! {
-    static ref RATELIMITER: Arc<Ratelimiter> = Arc::new(
-        Ratelimiter::builder(TOKEN_PER_SEC, Duration::from_millis(100))
-            .max_tokens(TOKEN_PER_SEC)
-            .initial_available(1)
-            .build()
-            .unwrap(),
-    );
 }
 
 /// Custom error type for HTTP requests.
@@ -38,33 +26,64 @@ pub enum RequestError {
     JsonError(String),
     #[error("Other error: {0}")]
     Other(String),
+    #[error("invalid header value: {0}")]
+    InvalidHeader(InvalidHeaderValue),
+}
+
+pub enum Method {
+    Get,
+    Put(Option<Vec<u8>>),
+    Post(Option<Vec<u8>>),
+    Delete,
 }
 
 /// Makes an HTTP request to the specified path with optional parameters.
-pub async fn request<T: DeserializeOwned>(
-    path: &str,                        // API path.
-    params: Option<Vec<(&str, &str)>>, // Optional query parameters.
+pub async fn request<'a, T: DeserializeOwned>(
+    method: Method,
+    path: &str,                   // API path.
+    params: HashMap<&str, &str>,  // Optional query parameters.
+    headers: HashMap<&str, &str>, // Optional header parameters.
+    token: Option<&str>,
 ) -> Result<T, RequestError> {
-    // Wait for rate limiter if necessary.
-    if let Err(sleep) = RATELIMITER.try_wait() {
-        std::thread::sleep(sleep);
-    }
-
     // Construct the URL.
-    let url = match params {
-        Some(params) => reqwest::Url::parse_with_params(&format!("{}{}", BASE_URL, path), &params)
-            .map_err(|e| RequestError::Other(e.to_string()))?,
-        None => reqwest::Url::parse(&format!("{}{}", BASE_URL, path))
-            .map_err(|e| RequestError::Other(e.to_string()))?,
+    let url = if params.len() > 0 {
+        reqwest::Url::parse_with_params(&format!("{}{}", BASE_URL, path), &params)
+            .map_err(|e| RequestError::Other(e.to_string()))?
+    } else {
+        reqwest::Url::parse(&format!("{}{}", BASE_URL, path))
+            .map_err(|e| RequestError::Other(e.to_string()))?
     };
 
-    // Get the API token from the environment variable.
-    let token = env::var("marketdata_token").map_err(|_| RequestError::TokenNotSet)?;
+    // Construct the request
+    let mut req: RequestBuilder;
+    match method {
+        Method::Get => req = CLIENT.get(url.as_str()),
+        Method::Delete => req = CLIENT.delete(url.as_str()),
+        Method::Post(body) => {
+            req = CLIENT.post(url.as_str());
+            if let Some(body) = body {
+                req = req.body(body);
+            }
+        }
+        Method::Put(body) => {
+            req = CLIENT.put(url.as_str());
+            if let Some(body) = body {
+                req = req.body(body);
+            }
+        }
+    }
 
-    // Send the HTTP request.
-    let response = CLIENT
-        .get(url.as_str())
-        .bearer_auth(token)
+    if let Some(token) = token {
+        req = req.bearer_auth(token)
+    }
+
+    if headers.len() > 0 {
+        for (k, v) in headers {
+            req = req.header(k, v);
+        }
+    }
+
+    let response = req
         .send()
         .await
         .map_err(|e| RequestError::Other(e.to_string()))?;
